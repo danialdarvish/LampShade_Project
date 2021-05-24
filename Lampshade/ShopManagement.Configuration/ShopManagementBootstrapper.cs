68d3f43ec01e70e32443111e9c02f331b8040696
danial_darvish <d.darvish80@gmail.com>
2021-05-25 01:18:29 +0430

    ProductCategory Management Implemented

---
+++ Lampshade/ShopManagement.Configuration/ShopManagementBootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+using ShopManagement.Infrastructure.EFCore;
+using ShopManagement.Infrastructure.EFCore.Repository;
+using SM.Application;
+
+namespace ShopManagement.Configuration
+{
+    public class ShopManagementBootstrapper
+    {
+        public static void Configure(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<IProductCategoryApplication, ProductCategoryApplication>();
+            services.AddTransient<IProductCategoryRepository, ProductCategoryRepository>();
+
+            services.AddDbContext<ShopContext>(x =>
+                x.UseSqlServer(connectionString));
+        }
+    }
+}
