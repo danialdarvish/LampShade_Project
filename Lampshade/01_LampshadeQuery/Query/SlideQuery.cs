6ee652bcd0eaf320e6b7ee19621e0aad58a0bfb3
danial_darvish <d.darvish80@gmail.com>
2021-05-28 17:32:26 +0430

    Slider in UI Implemented

---
+++ Lampshade/01_LampshadeQuery/Query/SlideQuery.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using _01_LampshadeQuery.Contracts.Slide;
+using ShopManagement.Infrastructure.EFCore;
+
+namespace _01_LampshadeQuery.Query
+{
+    public class SlideQuery : ISlideQuery
+    {
+        private readonly ShopContext _context;
+
+        public SlideQuery(ShopContext context)
+        {
+            _context = context;
+        }
+
+        public List<SlideQueryModel> GetSlides()
+        {
+            return _context.Slides.Where(x => !x.IsRemoved)
+                .Select(x => new SlideQueryModel
+                {
+                    Picture = x.Picture,
+                    PictureTitle = x.PictureTitle,
+                    PictureAlt = x.PictureAlt,
+                    BtnText = x.BtnText,
+                    Heading = x.Heading,
+                    Link = x.Link,
+                    Text = x.Text,
+                    Title = x.Title
+                }).ToList();
+        }
+    }
+}
