3a40385fb019c7a644287cb6cbaa4a28c915ecbf
danial_darvish <d.darvish80@gmail.com>
2021-06-02 18:12:30 +0430

    IInventory Application implemented

---
+++ Lampshade/InventoryManagement.Application/InventoryApplication.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using _0_Framework.Application;
+using InventoryManagement.Application.Contracts.Inventory;
+using InventoryManagement.Domain.InventoryAgg;
+using Microsoft.VisualBasic;
+
+namespace InventoryManagement.Application
+{
+    public class InventoryApplication : IInventoryApplication
+    {
+        private readonly IInventoryRepository _inventoryRepository;
+
+        public InventoryApplication(IInventoryRepository inventoryRepository)
+        {
+            _inventoryRepository = inventoryRepository;
+        }
+
+
+        public OperationResult Create(CreateInventory command)
+        {
+            var operation = new OperationResult();
+            if (_inventoryRepository.Exists(x => x.ProductId == command.ProductId))
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+
+            var inventory = new Inventory(command.ProductId, command.UnitPrice);
+            _inventoryRepository.Create(inventory);
+            _inventoryRepository.SaveChanges();
+
+            return operation.Succeed();
+        }
+
+        public OperationResult Edit(EditInventory command)
+        {
+            var operation = new OperationResult();
+            var inventory = _inventoryRepository.Get(command.Id);
+            if (inventory == null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            if (_inventoryRepository.Exists(x =>
+                x.ProductId == command.ProductId && x.Id != command.Id))
+                return operation.Failed(ApplicationMessages.DuplicatedRecord);
+
+            inventory.Edit(command.ProductId, command.UnitPrice);
+            _inventoryRepository.SaveChanges();
+
+            return operation.Succeed();
+        }
+
+        public OperationResult Increase(IncreaseInventory command)
+        {
+            var operation = new OperationResult();
+            var inventory = _inventoryRepository.Get(command.InventoryId);
+
+            if (inventory == null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            const long operatorId = 1;
+            inventory.Increase(command.Count, operatorId, command.Description);
+
+            _inventoryRepository.SaveChanges();
+            return operation.Succeed();
+        }
+
+        public OperationResult Reduce(ReduceInventory command)
+        {
+            var operation = new OperationResult();
+            var inventory = _inventoryRepository.Get(command.InventoryId);
+
+            if (inventory == null)
+                return operation.Failed(ApplicationMessages.RecordNotFound);
+
+            const long operatorId = 0;
+            inventory.Reduce(command.Count, operatorId, command.Description, operatorId);
+
+            _inventoryRepository.SaveChanges();
+            return operation.Succeed();
+        }
+
+        public OperationResult Reduce(List<ReduceInventory> command)
+        {
+            var operation = new OperationResult();
+            const long operatorId = 0;
+            foreach (var item in command)
+            {
+                var inventory = _inventoryRepository.GetBy(item.ProductId);
+                inventory.Reduce(item.Count, operatorId, item.Description, item.OrderId);
+
+            }
+            _inventoryRepository.SaveChanges();
+            return operation.Succeed();
+        }
+
+        public EditInventory GetDetails(long id)
+        {
+            return _inventoryRepository.GetDetails(id);
+        }
+
+        public List<InventoryViewModel> Search(InventorySearchModel searchModel)
+        {
+            return _inventoryRepository.Search(searchModel);
+        }
+    }
+}
