e02891ef7da9c18893cf0f01165238d5932ea669
danial_darvish <d.darvish80@gmail.com>
2021-06-08 01:26:05 +0430

    Footer and Menu moved to seperated View Component

--- Lampshade/01_LampshadeQuery/Contracts/ProductCategory/IProductCategoryQuery.cs
+++ Lampshade/01_LampshadeQuery/Contracts/ProductCategory/IProductCategoryQuery.cs
@@ -4,6 +4,8 @@ namespace _01_LampshadeQuery.Contracts.ProductCategory
 {
     public interface IProductCategoryQuery
     {
+        ProductCategoryQueryModel GetProductCategoryWithProducts(string slug);
         List<ProductCategoryQueryModel> GetProductCategories();
+        List<ProductCategoryQueryModel> GetProductCategoriesWithProducts();
     }
 }
