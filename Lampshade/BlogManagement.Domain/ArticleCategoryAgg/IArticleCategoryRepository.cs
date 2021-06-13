37db9478d757c2e964a01201b4d927cd9a1dd321
danial_darvish <d.darvish80@gmail.com>
2021-06-13 12:45:55 +0430

    Article Management implemented

--- Lampshade/BlogManagement.Domain/ArticleCategoryAgg/IArticleCategoryRepository.cs
+++ Lampshade/BlogManagement.Domain/ArticleCategoryAgg/IArticleCategoryRepository.cs
@@ -8,6 +8,7 @@ namespace BlogManagement.Domain.ArticleCategoryAgg
     public interface IArticleCategoryRepository : IRepository<long, ArticleCategory>
     {
         string GetSlugBy(long id);
+        List<ArticleCategoryViewModel> GetArticleCategories();
         EditArticleCategory GetDetails(long id);
         List<ArticleCategoryViewModel> Search(ArticleCategorySearchModel searchModel);
     }
